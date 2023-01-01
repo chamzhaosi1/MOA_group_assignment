32bcca4bb58ebe786aca38719c261bfa80b67abf
chamzhaosi <chamzhaosi0808@e.newera.my>
2023-01-01 13:11:26 +0800

    vertical sequence progress bar done

--- app/src/main/java/com/example/pairassginment/MainActivity.kt
+++ app/src/main/java/com/example/pairassginment/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.pairassginment
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.pairassginment.databinding.ActivityMainBinding
+import com.example.pairassginment.student.Dashboard
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var bindingMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMain.root)
+
+        bindingMain.signInButton.setOnClickListener{
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+        }
     }
+
 }
