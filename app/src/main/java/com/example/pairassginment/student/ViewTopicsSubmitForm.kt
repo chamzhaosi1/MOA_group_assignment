dc7587198ed9e00b317e71f4549b5aeaccf61822
chamzhaosi <chamzhaosi0808@e.newera.my>
2023-01-02 23:42:06 +0800

    create read topic submitted ui

---
+++ app/src/main/java/com/example/pairassginment/student/ViewTopicsSubmitForm.kt
@@ -0,0 +1,11 @@
+package com.example.pairassginment.student
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTopicsSubmitForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_topics_submit_form)
+    }
+}
