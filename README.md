14c44d3226f2b7e979a0c66f9679e5ce9120a189
chamzhaosi <chamzhaosi0808@e.newera.my>
2023-01-08 21:37:14 +0800

    connect with firebase and add readme to record it's crud operation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MOA_group_assignment
+
+# Note for CRUD Firestore Database
+
+## To get user id
+```kotlin
+ val uid:String = FirebaseAuth.getInstance().currentUser!!.uid;
+```
+
+## To set the document 
+```kotlin
+mDB.collection("Users").document(uid)
+```
+
+## To create the collection with own document 
+```kotlin
+mDB.collection("Users").document(uid)
+            .set(user)
+            .addOnCompleteListener {
+                Toast.makeText(this@Dashboard, "record added successfully", Toast.LENGTH_SHORT).show()
+            }
+```
+
--- app/src/main/java/com/example/pairassginment/student/Dashboard.kt
+++ app/src/main/java/com/example/pairassginment/student/Dashboard.kt
@@ -3,40 +3,53 @@ package com.example.pairassginment.student
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.pairassginment.R
 import com.example.pairassginment.databinding.ActivityDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.transferwise.sequencelayout.SequenceStep
+import java.util.Objects
 
 class Dashboard : AppCompatActivity() {
-//    internal var step1: SequenceStep? = null
-//    internal var step2: SequenceStep? = null
-//    internal var step3: SequenceStep? = null
-//    internal var step4: SequenceStep? = null
-//    internal var step5: SequenceStep? = null
-
     private lateinit var binding: ActivityDashboardBinding
+    private var mDB: FirebaseFirestore = FirebaseFirestore.getInstance();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val step1 = binding.firstStep as SequenceStep
-        val step3 = binding.thirdStep as SequenceStep
+//        val step1 = binding.firstStep as SequenceStep
+//        val step3 = binding.thirdStep as SequenceStep
+//
+//        //programatically activating
+//        step3.setActive(true)
+//        step3.setTitle("Active Step")
+//        step3.setAnchor("Date...")
+//        step3.setSubtitle("Subtitle of this step third.")
+//
+//        //programatically seting style to Title
+//        step3.setTitleTextAppearance(R.style.vertical_progress_bar_title)
+//
+//        binding.detialBtn.setOnClickListener{
+//            var intent = Intent(this, ListOfThreeTopic::class.java)
+//            startActivity(intent)
+//        }
 
-        //programatically activating
-        step3.setActive(true)
-        step3.setTitle("Active Step")
-        step3.setAnchor("Date...")
-        step3.setSubtitle("Subtitle of this step third.")
+        val uid:String = FirebaseAuth.getInstance().currentUser!!.uid;
+        Log.d("User id", uid)
 
-        //programatically seting style to Title
-        step3.setTitleTextAppearance(R.style.vertical_progress_bar_title)
+        val user:MutableMap<String, String> = HashMap()
+        user["uid"] = uid;
 
-        binding.detialBtn.setOnClickListener{
-            var intent = Intent(this, ListOfThreeTopic::class.java)
-            startActivity(intent)
-        }
+
+        mDB.collection("Users").document(uid)
+            .set(user)
+            .addOnCompleteListener {
+                Toast.makeText(this@Dashboard, "record added successfully", Toast.LENGTH_SHORT).show()
+            }
 
     }
 }
