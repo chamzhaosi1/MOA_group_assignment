2fce724c0af5c558efa5c657de306a0ed1e16278
chamzhaosi <chamzhaosi0808@e.newera.my>
2023-01-09 23:14:35 +0800

    create remaining activity and GUI

--- app/src/main/java/com/example/pairassginment/student/ListOfOtherDocuments.kt
+++ app/src/main/java/com/example/pairassginment/student/ListOfOtherDocuments.kt
@@ -3,10 +3,14 @@ package com.example.pairassginment.student
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pairassginment.R
+import com.example.pairassginment.databinding.ActivityListOfItemBinding
 
 class ListOfOtherDocuments : AppCompatActivity() {
+    private lateinit var binding: ActivityListOfItemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_of_other_documents)
+        binding = ActivityListOfItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
